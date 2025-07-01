78a3468b7bc62cea53dda63951bf415fe2fb974b
shaily394 <shivanjalishaily@gmail.com>
2025-07-01 23:40:59 +0530

    Time: 93 ms (78.97%) | Memory: 13.9 MB (57.76%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int i ,int j , vector<int> &nums, vector<vector<int>> &dp){
+   /* int f(int i ,int j , vector<int> &nums, vector<vector<int>> &dp){
         if(i>j) return 0;
         if(dp[i][j] != -1 ) return dp[i][j];
         int coins =0;
@@ -10,13 +10,27 @@ public:
             maxi = max(maxi,coins);
         }
         return dp[i][j]=maxi;
-    }
+    }*/
 
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
-        return f(1,n,nums,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        
+       
+        for(int i =n;i >= 1; i--){
+            for(int j =1;j<=n;j++ ){
+                if(i>j) continue;
+                int maxi = INT_MIN;
+                for(int ind=i;ind<=j;ind++){
+                int coins = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1]+dp[ind+1][j];
+                maxi = max(maxi,coins);
+            }
+            dp[i][j]=maxi;
+            }
+            
+        }
+        return dp[1][n];
     }
 };
