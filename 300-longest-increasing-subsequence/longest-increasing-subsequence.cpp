c2bdedaf7678e222c45179e7bab6d6f95e60f3f2
shaily394 <shivanjalishaily@gmail.com>
2025-07-01 13:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int f(int ind,int prev_ind,vector<int> &nums ,  vector<vector<int>> &dp){
         if(ind==nums.size()) return 0;
@@ -14,5 +14,24 @@ public:
         int n = nums.size();
         vector<vector<int>> dp(n,vector<int>(n+1,-1)); //coordinate change for prev_ind
         return f(0,-1,nums,dp);
+    }
+}; */
+
+class Solution{
+public:
+    int lengthOfLIS(vector<int>& nums){
+        vector<int> temp;
+        int n = nums.size();
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+            }
+        }
+        return temp.size();
     }
 };
