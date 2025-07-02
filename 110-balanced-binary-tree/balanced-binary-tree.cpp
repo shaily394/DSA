b292ab9ca7d666232f15e29e086ac9d8d85de355
shaily394 <shivanjalishaily@gmail.com>
2025-07-02 22:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (22.99%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -14,19 +14,13 @@ public:
     int maxDepth(TreeNode* root) {
         if(root== NULL) return 0;
         int lh= maxDepth(root->left);
+        if(lh== -1) return -1;
         int rh = maxDepth(root->right);
+        if(rh == -1) return -1;
+        if(abs(rh-lh) >1) return -1;
         return 1+max(lh,rh);
     }
     bool isBalanced(TreeNode* root) {
-        if(root== NULL) return true;
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
-        if(abs(lh-rh) > 1) return false;
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-
-        if(!left || !right) return false;
-
-        return true;
+       return maxDepth(root) != -1;
     }
 };
