aca517a0cadac68111474b90f96249b5d914b0cb
shaily394 <shivanjalishaily@gmail.com>
2025-07-05 22:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (92.92%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -27,7 +27,7 @@ public:
     }
 };*/
 
-class Solution {
+/*class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root){
         vector<int> ans;
@@ -44,4 +44,36 @@ public:
         }
         return ans;
     }
+};*/
+
+class Solution{
+public:
+    vector<int> preorderTraversal(TreeNode* root){
+        TreeNode* curr = root;
+        vector <int> preorder;
+        while(curr!= NULL){
+            if(curr->left == NULL){
+                preorder.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    preorder.push_back(curr->val);
+                    curr= curr->left;
+                }
+                else {
+                    prev->right = NULL;
+                   
+                    curr = curr->right;
+                }
+            }
+        }
+        return preorder;
+    }
+
 };
