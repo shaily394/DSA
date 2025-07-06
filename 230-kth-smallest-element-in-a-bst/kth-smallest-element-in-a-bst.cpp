6df28c75df817108d04a0a519659765287198e87
shaily394 <shivanjalishaily@gmail.com>
2025-07-06 17:58:21 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (89.06%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -9,7 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+ //recursive
+/*class Solution {
 public:
     void f(TreeNode* root, int k, int &s,int &ans){
         if(root == NULL) return ;
@@ -29,4 +30,44 @@ public:
         f(root,k,s,ans);
         return ans;
     }
+};*/
+
+//morris
+
+class Solution{
+public:
+    int kthSmallest(TreeNode* root, int k){
+        
+        TreeNode * cur = root;
+        int s=0;
+        int ans;
+        while(cur != NULL){
+            if(cur->left == NULL){
+                s++;
+                if(s==k) ans= cur->val;
+                cur = cur->right;
+                }
+            else{
+                TreeNode* prev = cur->left;
+                while(prev->right != NULL && prev->right != cur){
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    s++;
+                    if(s==k) ans =cur->val;
+                    cur = cur->right;
+                }
+            }
+        }
+       
+        return ans;
+
+    }
+
 };
+
