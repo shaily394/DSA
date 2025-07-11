09bbf82402273941001e72c62a1b114c10467d7b
shaily394 <shivanjalishaily@gmail.com>
2025-07-11 21:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (51.11%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
        unordered_map<int , int> mpp;
@@ -15,5 +15,35 @@ public:
     }
         return {};
     
+    }
+};*/
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<pair<int, int>> numbersWithIndices;
+        for (int i = 0; i < numbers.size(); i++) {
+            numbersWithIndices.push_back({numbers[i], i});
+        }
+        
+        sort(numbersWithIndices.begin(), numbersWithIndices.end());
+        
+        int left = 0;
+        int right = numbers.size() - 1;
+        
+        while (left < right) {
+            int sum = numbersWithIndices[left].first + numbersWithIndices[right].first;
+            if (sum == target) {
+                return {numbersWithIndices[left].second, numbersWithIndices[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return {}; 
     }
 };
