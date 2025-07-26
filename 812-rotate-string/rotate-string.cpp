fb79914dabb15185a378a327b2cb4b30912e9512
shaily394 <shivanjalishaily@gmail.com>
2025-07-26 09:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (38.03%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()){
@@ -7,6 +7,22 @@ public:
         for(int i =0;i<s.size();i++){
             string rot = s.substr(i) + s.substr(0,i);
             if(rot == goal) return true;
+        }
+        return false;
+    }
+};*/
+
+class Solution {
+public:
+    bool rotateString(string s, string goal){
+        if(s.size() != goal.size()){
+            return false;
+        }
+        string temp= s+s;
+        int find=temp.find(goal);
+        if(find>=0)
+        {
+            return true;
         }
         return false;
     }
