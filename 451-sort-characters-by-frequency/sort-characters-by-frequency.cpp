69fe2edd48b19b50c3ed7b2beed9c94178c6028a
shaily394 <shivanjalishaily@gmail.com>
2025-07-28 15:35:57 +0530

    Time: 7 ms (20.65%) | Memory: 16.9 MB (7.90%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,5 +1,5 @@
 
-class Solution {
+/*class Solution {
 public:
    
     string frequencySort(string s) {
@@ -12,5 +12,29 @@ public:
             return mpp[a] > mpp[b];
         });
         return s;
+    }
+};*/
+//bucket sort
+class Solution{
+public:
+    string frequencySort(string s){
+        unordered_map<char,int> mpp;
+        for(auto ch : s){
+            mpp[ch]++;
+        }
+        vector<vector<char>> freq(s.size()+1);
+        for (auto it : mpp) {
+            char ch = it.first;
+            int count = it.second;
+            freq[count].push_back(ch);  
+        }
+        string ans = "";
+        for (int fq = s.length(); fq >= 1; fq--) {
+            for (auto ch :freq[fq]) {
+                ans.append(fq, ch);  
+            }
+        }
+
+        return ans;
     }
 };
