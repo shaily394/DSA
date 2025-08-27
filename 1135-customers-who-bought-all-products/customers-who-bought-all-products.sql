247d891cc2f1b90a488ca22415acfde68bfdee10
Shivanjali Shaily <shivanjalishaily@gmail.com>
2025-08-27 12:26:45 +0530

    Time: 612 ms (31.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select c.customer_id 
+from Customer c inner join Product p 
+on c.product_key = p.product_key
+group by c.customer_id having count(distinct c.product_key) = (select count(distinct product_key) from Product);
