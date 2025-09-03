6fa6d2d1803a607b6322679fd195e69729d9b2bd
Shivanjali Shaily <shivanjalishaily@gmail.com>
2025-09-03 17:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.07%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int count0 = 0 , count1 = 0 , count2 = 0;
@@ -11,5 +11,28 @@ public:
         for(int i = 0 ; i < count0 ; i++) nums[i] = 0;
         for(int i = count0 ; i < count1 + count0 ; i ++) nums[i] = 1;
         for(int i = count1 + count0 ; i < nums.size() ; i++) nums[i] = 2;
+    }
+}; */
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+        int low = 0 , mid = 0 , high = n -1 ;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
+                mid++ ;
+                low++;
+            }
+            else if(nums[mid] == 1) mid++;
+            else{
+                swap(nums[mid] , nums[high]);
+                high --;
+
+            }
+        }
+
     }
 };
